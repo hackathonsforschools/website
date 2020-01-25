412dc6937b033da08c3eaee21d9d739876d84b62
Daniel Spencer <danielfspencer1@gmail.com>
2020-01-25 23:02:42 +0000

    Make calendar more responsive

--- assets/js/calendar.js
+++ assets/js/calendar.js
@@ -1,6 +1,8 @@
 const CALENDAR_ID = 't8iphvpqadetnf7ccpb2c7pnjk@group.calendar.google.com'
 const CALENDAR_KEY = 'AIzaSyAQqb5ZhZIOroU98W1_xzXlN3jWN8Ta4aQ'
 
+const SMALL_DEVICE_WIDTH = 600
+
 const DATE_24HR_FORMAT = {
   hour: '2-digit',
   minute: '2-digit',
@@ -13,9 +15,14 @@ let calendar
 document.addEventListener('DOMContentLoaded', () => {
   let calendarElement = document.querySelector('#calendar')
 
+  let defaultView = 'dayGridMonth'
+  if (window.innerWidth < SMALL_DEVICE_WIDTH) {
+    defaultView = 'listYear'
+  }
+
   calendar = new FullCalendar.Calendar(calendarElement, {
     plugins: ['dayGrid', 'googleCalendar', 'list', 'timeGrid'],
-    defaultView: 'dayGridMonth',
+    defaultView: defaultView,
     header: { center: 'dayGridMonth, timeGridWeek, listYear'},
     titleFormat: { year: 'numeric', month: 'short', day: 'numeric' },
     buttonText: { dayGridMonth:'Month', timeGridWeek:'Week', list:'All', today:'Today'},
